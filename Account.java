19016533dcd1f316d24adba5007e576f22620a4b
LindaJebet <42126672+LindaJebet@users.noreply.github.com>
2018-08-21 00:19:20 -0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,31 @@
+package com.example.BinaryCoders.famtree;
+
+import android.media.Image;
+
+public class Account {
+
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+    private Image profilePicture;
+    private Profile myProfile;
+
+    public Account(){
+
+    }
+
+    public boolean ChangePassword(String oldPassword, String newPassword){
+
+        return false;
+    }
+
+    public boolean ChangeEmail(String email, String password ){
+
+        return false;
+    }
+
+
+
+}
